1a120e4221506d6c4c729c0c6da4976d7a931722
Orgha Tomal Roy <orgha.tomal@welldev.io>
2025-03-04 14:18:25 +0600

    added cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import router from "./routes.mjs";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://rhb-poc.netlify.app"],
+    credentials: true,
+    method: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use("/api", router);
 
